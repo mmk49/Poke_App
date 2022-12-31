3128bb89a2de5a1a7474cf163a8962e5cb33f858
mmk49 <mustafakhan524@yahoo.com>
2022-12-31 15:58:38 -0800

    Setup

---
+++ Poke_AppV1/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.example.poke_appv1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.poke_appv1 to javafx.fxml;
+    exports com.example.poke_appv1;
+}
