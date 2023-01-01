deca4d36885e4686ee77ed82abc393dda1ccbd85
mmk49 <mustafakhan524@yahoo.com>
2022-12-31 17:00:38 -0800

    controller working

--- Poke_AppV1/src/main/java/com/example/poke_appv1/HelloController.java
+++ Poke_AppV1/src/main/java/com/example/poke_appv1/HelloController.java
@@ -1,14 +1,39 @@
 package com.example.poke_appv1;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label idLabel;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label numberLabel;
+
+    @FXML
+    private Label pokedexNumberLabel;
+
+    @FXML
+    private ImageView pokemonImage;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TextField searchText;
+
+    @FXML
+    void OnActionSearch(ActionEvent event) {
+
     }
+
 }
+
