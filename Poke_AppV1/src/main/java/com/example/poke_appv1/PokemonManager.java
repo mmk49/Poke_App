b270f0f7287ed4edad7547b0265b8ebdcb6c7527
mmk49 <mustafakhan524@yahoo.com>
2022-12-31 18:22:13 -0800

    PokemonManager Class added

---
+++ Poke_AppV1/src/main/java/com/example/poke_appv1/PokemonManager.java
@@ -0,0 +1,43 @@
+package com.example.poke_appv1;
+
+public class PokemonManager {
+    private final String id;
+    private final String name;
+    private final String nationalPokedexNumbers;
+    private final String number;
+    private final String smallImage;
+    private final String largeImage;
+
+    public PokemonManager(String id, String name, String nationalPokedexNumbers, String number, String smallImage, String largeImage) {
+        this.id = id;
+        this.name = name;
+        this.nationalPokedexNumbers = nationalPokedexNumbers;
+        this.number = number;
+        this.smallImage = smallImage;
+        this.largeImage = largeImage;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNationalPokedexNumbers() {
+        return nationalPokedexNumbers;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getSmallImage() {
+        return smallImage;
+    }
+
+    public String getLargeImage() {
+        return largeImage;
+    }
+}
